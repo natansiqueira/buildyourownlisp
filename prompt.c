8786dc6cc85f52cf51cb2eaf25bd6ce5dc67d67a
Natan Siqueira Alves <natan.siqueira@outlook.com.br>
2021-11-01 22:44:53 -0300

    Add REPL using readline and history

--- prompt.c
+++ prompt.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
   /* Print Version and Exit Information */
@@ -10,14 +12,17 @@ int main(int argc, char** argv) {
   /* In never ending loop */
   while(1) {
 
-    /* Output our prompt */
-    fputs("lispy> ", stdout);
-    
-    /* Read a line of user input of maximum size 2048 */
-    fgets(input, 2048, stdin);
+    /* Output our prompt and get input */
+    char* input = readline("lispy> ");
+
+    /* Add input to history */
+    add_history(input);
 
     /* Echo input back to user */
-    printf("No you're a %s", input);
+    printf("No you're a %s\n", input);
+
+    /* Free retrieved input */
+    free(input);
   }
 
   return 0;
